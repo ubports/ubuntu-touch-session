7bd60923dfaf39ff32128b2f2917f988398c5074
Oliver Grawert <ogra@ubuntu.com>
2013-07-17 10:41:29 +0200

    add dbus session handling, fix up /upstart-phablet.sh

--- etc/profile.d/upstart-phablet.sh
+++ etc/profile.d/upstart-phablet.sh
@@ -1,2 +1,5 @@
 # Join the phablet Upstart session
+uid=$(getent passwd "$USER"|cut -d: -f3)
+export XDG_RUNTIME_DIR="/run/user/$uid"
+
 export UPSTART_SESSION=$(/sbin/initctl list-sessions | awk '{ print $NF; quit }')
