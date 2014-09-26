47146540242a8c3ef881f6e8e8aa414816891e63
Oliver Grawert <ogra@ubuntu.com>
2014-09-26 16:41:54 +0200

    oops, do not truncate the file we want to read (and drop the code for the obsolete file, it should be gone everywhere by now)

--- etc/profile.d/dbus-source.sh
+++ etc/profile.d/dbus-source.sh
@@ -1,6 +1,3 @@
-# truncate obsolete ~/.dbus-session file if it exists
-[ -e $XDG_RUNTIME_DIR/dbus-session ] && echo >$XDG_RUNTIME_DIR/dbus-session
-
 # source dbus address from new location
 [ -e $XDG_RUNTIME_DIR/dbus-session ] && . $XDG_RUNTIME_DIR/dbus-session
 [ -n "$DBUS_SESSION_BUS_ADDRESS" ] && export DBUS_SESSION_BUS_ADDRESS
